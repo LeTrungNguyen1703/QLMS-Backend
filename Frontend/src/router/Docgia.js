a4f87ecc829c2dce7b5ab463b9ac92b03941677c
Nguyen <nguyenc2300110@student.ctu.edu.vn>
2025-09-15 14:16:44 +0700

    Fix something

--- Frontend/src/router/Docgia.js
+++
@@ -1,7 +0,0 @@
-import TableDG from "@/components/pages/tables/TableDG.vue";
-import CreateDG from "@/components/pages/creates/CreateDG.vue";
-
-export const Docgia = [
-    {path: "/table-dg", name: "TableDG", component: TableDG},
-    {path: "/create-dg", name: "CreateDG", component: CreateDG},
-];
